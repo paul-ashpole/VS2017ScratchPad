24c1ea7f659548468cb2a9df12cd4abb651a3e7d
Paul Ashpole <paul.ashpole@swinton.co.uk>
2017-04-03 16:07:52 +0000

    Live unit testing

---
+++ Scratchpad/Adder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scratchpad
+{
+    public class Adder
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ ScratchpadTests/AdderTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Scratchpad.Tests
+{
+    [TestClass()]
+    public class AdderTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.AreEqual(4, new Adder().Add(2, 2));
+        }
+    }
+}
