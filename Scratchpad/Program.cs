b72425106a8289a7d2e83b44a72713c8df8ec45b
Paul Ashpole <paul.ashpole@swinton.co.uk>
2017-04-03 14:51:45 +0000

    Added C# 7.0 examples

---
+++ Scratchpad/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Scratchpad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 0b1, 0b1_0, 0b100, 0b10_00 }; //binary literals and numerical seperators
+
+            var (sum, count) = Tally(numbers);  //need nuget for System.ValueTuple
+
+            Console.WriteLine($"Sum: {sum}, count: {count}");
+            Console.ReadLine();
+        }
+
+        private static (int sum, int count) Tally(int[] values)
+        {
+            var r = (s: 0, c: 0);
+            foreach (var v in values)
+            {
+                Add(v, 1);
+            }
+            return r;
+
+            void Add(int s, int c) { r.s += s; r.c += c; }  //local function
+        }
+    }
+}
